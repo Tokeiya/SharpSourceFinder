bd07fedd79abb03464bfd16f99933a34cc7528c2
Hikotaro Abe <netseed@seedcreate.onmicrosoft.com>
2020-08-21 20:23:35 +0900

    clean up code.

--- SharpSourceFinderCore/Assembly.cs
+++ SharpSourceFinderCore/Assembly.cs
@@ -1,5 +1,4 @@
 ﻿using System.Runtime.CompilerServices;
 
-[assembly:InternalsVisibleTo("Playground")]
-[assembly:InternalsVisibleTo("SharpSourceFinderCoreTests")]
-
+[assembly: InternalsVisibleTo("Playground")]
+[assembly: InternalsVisibleTo("SharpSourceFinderCoreTests")]
