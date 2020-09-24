63209587c1f9c959954d25b078c2d57d5408afc1
Hikotaro Abe <netseed@seedcreate.onmicrosoft.com>
2020-09-24 22:49:07 +0900

    Implement ImaginaryRootTest

---
+++ SharpSourceFinderCoreTests/ImaginaryRootTest.cs
@@ -0,0 +1,178 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+using Xunit.Abstractions;
+using static Xunit.Assert;
+using ChainingAssertion;
+using Tokeiya3.SharpSourceFinderCore;
+
+namespace SharpSourceFinderCoreTests
+{
+	public class ImaginaryRootTest
+	{
+		private readonly ITestOutputHelper _output;
+
+		public ImaginaryRootTest(ITestOutputHelper output) => _output = output;
+
+
+		[Trait("TestLayer", nameof(ImaginaryRoot))]
+		[Fact]
+		public void ParentGetterTest() => Throws<NotSupportedException>(() => ImaginaryRoot.Root.Parent);
+
+		[Trait("TestLayer", nameof(ImaginaryRoot))]
+		[Fact] 
+		public void StorageGetterTest() => Throws<NotSupportedException>(() => ImaginaryRoot.Root.Storage);
+
+		[Trait("TestLayer", nameof(ImaginaryRoot))]
+		[Fact]
+		public void RegisterChildTest()
+		{
+			var sample = new NameSpace(new PhysicalStorage("hoge"));
+			Throws<NotSupportedException>(() => ImaginaryRoot.Root.RegisterChild(sample));
+		}
+
+		[Trait("TestLayer", nameof(ImaginaryRoot))]
+		[Fact]
+		public void DescribeWithStringBuilderTest()
+		{
+			var bld = new StringBuilder();
+
+			ImaginaryRoot.Root.Describe(bld, "  ", 0);
+			bld.ToString().IsEmpty();
+		}
+
+		[Trait("TestLayer", nameof(ImaginaryRoot))]
+		[Fact]
+		public void StringReturnsDescribeTest()
+		{
+			ImaginaryRoot.Root.Describe().IsEmpty();
+			ImaginaryRoot.Root.Describe(" ").IsEmpty();
+		}
+
+		[Trait("TestLayer", nameof(ImaginaryRoot))]
+		[Fact]
+		public void AncestorsTest() => ImaginaryRoot.Root.Ancestors().IsEmpty();
+
+		[Trait("TestLayer", nameof(ImaginaryRoot))]
+		[Fact]
+		public void AncestorsAndSelfTest() => Throws<NotSupportedException>(() => ImaginaryRoot.Root.AncestorsAndSelf().Any());
+
+		[Trait("TestLayer", nameof(ImaginaryRoot))]
+		[Fact]
+		public void ChildrenTest() => Throws<NotSupportedException>(() => ImaginaryRoot.Root.Children().Any());
+
+		[Trait("TestLayer", nameof(ImaginaryRoot))]
+		[Fact]
+		public void DescendantsTest() => Throws<NotSupportedException>(() => ImaginaryRoot.Root.Descendants().Any());
+
+		[Trait("TestLayer", nameof(ImaginaryRoot))]
+		[Fact]
+		public void DescendantsAndSelfTest() => Throws<NotSupportedException>(() => ImaginaryRoot.Root.DescendantsAndSelf().Any());
+
+		[Trait("TestLayer", nameof(ImaginaryRoot))]
+		[Fact]
+		public void GetQualifiedNameTest()
+		{
+			object? dmy=null;
+			Throws<NotSupportedException>(() => dmy = ImaginaryRoot.Root.GetQualifiedName());
+
+			dmy.IsNull();
+		}
+
+		[Trait("TestLayer", nameof(ImaginaryRoot))]
+		[Fact]
+		public void AggregateIdentitiesTest()
+		{
+			var stack=new Stack<(IdentityCategories,string)>();
+			ImaginaryRoot.Root.AggregateIdentities(stack);
+
+			stack.IsEmpty();
+
+		}
+
+		[Trait("TestLayer", nameof(ImaginaryRoot))]
+		[Fact]
+		public void IsLogicallyEquivalentToTest()
+		{
+			var x = ImaginaryRoot.Root;
+			var y = ImaginaryRoot.Root;
+			var z = ImaginaryRoot.Root;
+
+
+			//Symmetrically
+			x.IsLogicallyEquivalentTo(x).IsTrue();
+
+
+			//Transitively
+			x.IsLogicallyEquivalentTo(y).IsTrue();
+			y.IsLogicallyEquivalentTo(z).IsTrue();
+			x.IsLogicallyEquivalentTo(z).IsTrue();
+
+			//Reflexively
+			x.IsLogicallyEquivalentTo(x).IsTrue();
+
+			//Inequivalent
+			var a=new NameSpace();
+			x.IsLogicallyEquivalentTo(a).IsFalse();
+			x.IsPhysicallyEquivalentTo(a).IsFalse();
+
+			a.IsLogicallyEquivalentTo(x).IsFalse();
+			a.IsPhysicallyEquivalentTo(x).IsFalse();
+
+		}
+
+		[Trait("TestLayer", nameof(ImaginaryRoot))]
+		[Fact]
+		public void IsPhysicallyEquivalentToTest()
+		{
+			var x = ImaginaryRoot.Root;
+			var y = ImaginaryRoot.Root;
+			var z = ImaginaryRoot.Root;
+
+
+			//Symmetrically
+			x.IsLogicallyEquivalentTo(x).IsTrue();
+			x.IsPhysicallyEquivalentTo(x).IsTrue();
+
+			//Transitively
+			x.IsLogicallyEquivalentTo(y).IsTrue();
+			x.IsPhysicallyEquivalentTo(y).IsTrue();
+
+			y.IsLogicallyEquivalentTo(z).IsTrue();
+			y.IsPhysicallyEquivalentTo(z).IsTrue();
+
+			x.IsLogicallyEquivalentTo(z).IsTrue();
+			y.IsPhysicallyEquivalentTo(z).IsTrue();
+
+			//Reflexively
+			x.IsLogicallyEquivalentTo(x).IsTrue();
+			x.IsPhysicallyEquivalentTo(x).IsTrue();
+
+			//Inequivalent
+			var a = new NameSpace();
+			x.IsLogicallyEquivalentTo(a).IsFalse();
+			a.IsLogicallyEquivalentTo(x).IsFalse();
+
+		}
+
+		[Trait("TestLayer", nameof(ImaginaryRoot))]
+		[Fact]
+		public void IsImaginaryRootTest()
+		{
+			IDiscriminatedElement sample = ImaginaryRoot.Root;
+			ImaginaryRoot.IsImaginaryRoot(sample).IsTrue();
+
+			sample = new NameSpace();
+			ImaginaryRoot.IsImaginaryRoot(sample).IsFalse();
+
+		}
+
+
+
+
+
+	}
+
+}
