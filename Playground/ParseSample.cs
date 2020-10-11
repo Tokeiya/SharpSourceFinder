632320df93084d88d83654d50e0ccc4f9798f298
Hikotaro Abe <netseed@seedcreate.onmicrosoft.com>
2020-10-11 21:32:01 +0900

    Implements InterfaceElement class. Test passed.

---
+++ Playground/ParseSample.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using data=System.Data;
+using static System.Console;
+
+
+namespace Playground
+{
+	using System.IO;
+
+	namespace InnerNameSpace
+	{
+		public class InnerClass
+		{
+
+		}
+	}
+
+	namespace Hoge.Moge
+	{
+		public class HogeMoge
+		{
+
+		}
+	}
+	public class ParseSample
+	{
+	}
+}
