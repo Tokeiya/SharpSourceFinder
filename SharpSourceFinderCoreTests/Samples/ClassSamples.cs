65df540940b987ee01060018c1ef9456975d24c5
Tokeiya3 <netseed@seedcreate.onmicrosoft.com>
2020-10-18 13:43:30 +0900

    add class samples.

---
+++ SharpSourceFinderCoreTests/Samples/ClassSamples.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NameSpace
+{
+	public class Public
+	{
+
+	}
+
+	internal class Internal
+	{
+
+	}
+
+	public unsafe class Unsafe
+	{
+
+	}
+
+	public partial class Partial
+	{
+
+	}
+
+	public abstract class Abstract
+	{
+
+	}
+
+	public sealed class Sealed
+	{
+
+	}
+
+	public unsafe sealed partial class UnsafePartialSealed
+	{
+
+	}
+
+	public class Envelope
+	{
+		private class Private
+		{
+
+		}
+
+		protected internal class ProtectedInternal
+		{ }
+
+		private protected class PrivateProtected
+		{ }
+
+		protected class Protected
+		{
+
+		}
+	}
+}
